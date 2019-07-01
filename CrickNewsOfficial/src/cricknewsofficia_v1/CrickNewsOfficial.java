f3d82e45ba49f3f9c69cf3491dfcee4c1079252f
Mihindu Ranasinghe <37303081+MihinduRanasinghe@users.noreply.github.com>
2019-07-01 11:24:23 +0530

    added solid principles

---
+++ CrickNewsOfficial/src/cricknewsofficia_v1/CrickNewsOfficial.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cricknewsofficia_v1;
+
+/**
+ *
+ * @author Mihindu Ranasinghe
+ */
+public class CrickNewsOfficial {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ CrickNewsOfficial/src/cricknewsofficia_v1/DbConnection.java
@@ -0,0 +1,24 @@
+
+package cricknewsofficia_v1;
+
+import java.sql.*;
+
+class DbConnection {
+    public Connection cn;
+    public Connection createScreen (){
+        try{
+           Class.forName("com.mysql.jdbc.Driver");
+            String dbpath="jdbc:mysql://localhost:3308/cricknews";
+            cn=DriverManager.getConnection(dbpath,"root","");
+        }
+        catch(ClassNotFoundException ex1){
+            System.out.println(ex1.getMessage());
+            
+        }
+        catch(SQLException ex2){
+            System.out.println(ex2.getMessage());
+        }
+        return cn;
+    }
+    
+}
