2a3794f4b20ebaee8fa3912e7624ed3321d78919
Mihindu Ranasinghe <37303081+MihinduRanasinghe@users.noreply.github.com>
2019-07-06 14:13:52 +0530

    Removed unwanted comments

--- CrickNewsOfficial/src/cricknewsofficial_V2/Repository/SqlRepository.java
+++ CrickNewsOfficial/src/cricknewsofficial_V2/Repository/SqlRepository.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package cricknewsofficial_V2.Repository;
 
 import cricknewsofficial_V2.DatabaseClass.CricketTeams;
